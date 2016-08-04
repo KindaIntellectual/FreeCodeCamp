3cf8053e139ceda99d951ebd5369cbbcac9a25d4
Raven <KindaIntellectual@users.noreply.github.com>
2016-08-04 14:27:18 -0400

    Complete - Mutations.js

--- Challenges/Mutations.js
+++ Challenges/Mutations.js
@@ -1,20 +1,17 @@
 
 function mutation(arr) {
-  var splitArrOne = arr[0].split('');
-  var splitArrTwo = arr[1].split('');
-  var checkArr = [];
-  for (var i=0; i<arr[1].length; i++){
-    if (arr[1].toLowerCase().indexOf(arr[0].toLowerCase()) !== -1){
-      checkArr.push(splitArrOne[i]);
+  var splitIt = arr[1].split('');
+  var chkArr = [];
+  for (var i=0; i<splitIt.length; i++){
+    if (arr[0].toLowerCase().indexOf(splitIt[i].toLowerCase()) > -1){
+      chkArr.push(splitIt[i]);
     }
   }
- // var dupeChk = checkArr.filter(function(x,y){
- //   return checkArr.indexOf(x.toLowerCase()) == y;
- // });
-  if (checkArr.join('').toLowerCase() !== arr[1].toLowerCase()){
-    return false;
+  var joinArr = chkArr.join('');
+  if (joinArr == arr[1]){
+    return true;
   }
-  return true;
+  return false;
 }
 
-mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]);
+mutation(["hello", "hey"]);
