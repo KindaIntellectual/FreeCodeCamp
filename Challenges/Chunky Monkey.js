5152c02e219a9bcd9ff1fb3462e2d1cbac14e540
Raven <KindaIntellectual@users.noreply.github.com>
2016-08-04 09:28:22 -0400

    Complete - Chunky Monkey.js

--- Challenges/Chunky Monkey.js
+++ Challenges/Chunky Monkey.js
@@ -1,13 +1,14 @@
 
 function chunkArrayInGroups(arr, size) {
   // Break it up.
-  var arrLength = (arr.length) - size;
+  var arrLength = (arr.length);
   var someArr = new Array();
   for (var i=0; i < arrLength; i++){
     var splicedIt = arr.splice(0, size);
     someArr.push(splicedIt);
   }
   //someArr.pop();
+  someArr = someArr.filter(function(elem) {return elem.length > 0;});
   return someArr;
 }
 
